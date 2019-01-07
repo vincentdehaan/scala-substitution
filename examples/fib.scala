19e9ef83602190235b98f66c23b4712e1aaf1e9a
Vincent de Haan <vincentdehaan@gmail.com>
2019-01-07 07:19:57 +0100

    Working example of Fibonacci series

--- examples/fib.scala
+++ examples/fib.scala
@@ -3,8 +3,13 @@ import SubstitutionMacros._
 object Fib extends App {
   implicit val pr = new SubstitutionMacros.Printer
   substitute {
-    def count(x: Int): Int = if(x == 0) 0 else count(x - 1)// + 1
-    val x = count(4)
+    def count(x: Int): Int = if(x == 0) 0 else count(x - 1) + 1
+    val x = count(2)
+  }
+
+  substitute {
+    def fib(x: Int): Int = if(x <= 1) 1 else fib(x - 1) + fib(x - 2)
+    val y = fib(4)
   }
 }
 
--- examples/macros.scala
+++ examples/macros.scala
@@ -9,14 +9,16 @@ object SubstitutionMacros {
   def substituteImpl[A](c: Context)(expr: c.Expr[A]): c.Expr[A] = {
     import c.universe._
 
-    val defs = mutable.Map[String, String]()
+    val defs = mutable.Map[String, (String, String)]() // (defName, (defBody, firstArg))
 
     object definitionSearcher extends Transformer {
       override def transform(tree: Tree): Tree = tree match {
-        case q"def $tname($arg1): $tpe = $expr" => // TODO: why does this not match?
-          println(s"${tname.toString} = ${expr.toString}")
-          defs(tname.toString) = expr.toString
-          tree
+        case q"def $tname($arg1): $tpe = $expr" =>
+          arg1 match {
+            case ValDef(_, TermName(arg1name), _, _) =>
+              defs(tname.toString) = (expr.toString, arg1name)
+              tree
+          }
         case _ => super.transform(tree)
       }
     }
@@ -24,33 +26,30 @@ object SubstitutionMacros {
     object substitionTransformer extends Transformer {
       val defTable = scala.collection.mutable.Map[String, String]()
       override def transform(tree: Tree): Tree = tree match {
-          case q"$f($arg1)" if !f.toString.contains(".") => // TODO: get rid of this ugly workardound!
-            val t = q"""
-                        val defs = Map[String, String]()
-             printall($arg1)($f, ${f.toString}, ${defs(f.toString)}, ${arg1.toString})"""
-           c.typecheck(t) match {
-             case Block(_, print) => print
-           }
-
-          case _ => super.transform(tree)
+        case q"$f($arg1)" if !f.toString.contains(".") => // TODO: get rid of this ugly workardound!
+          val t = q"""
+           printall($arg1)($f, ${f.toString}, ${defs(f.toString)._1}, ${defs(f.toString)._2})
+            """
+          c.typecheck(t)
+        case _ => super.transform(tree)
       }
     }
-    val d = definitionSearcher.transform(expr.tree) // Do we need to save this?
+
+    definitionSearcher.transform(expr.tree)
 
     val t = substitionTransformer.transform(expr.tree)
-    println(substitionTransformer.defTable)
-    val defTableExpr = c.typecheck(
-      Block(q"""
-         val defs = scala.collection.mutable.Map[String, String]()
-       """ :: t :: substitionTransformer.defTable.map(df => q"defs(${df._1}) = ${df._2}").toList, q"println(5)")
-    )
-    println(showRaw(defTableExpr))
-    c.Expr[A](defTableExpr)
+    c.Expr[A](t)
   }
 
   class Printer {
-    var indent: Int = 0;
-    def print(s: String): Unit = println((" " * indent) + s.split("\n").mkString(" " * indent))
+    var indent: Int = -1;
+    var previndent: Int = -1;
+    // TODO: reduce spaces in output
+    def print(s: String): Unit = {
+      val boxchar = if(previndent > indent) "\u2514" else ""
+      println(("\u2502 " * indent) + boxchar + reduceWhitespace(s))
+      previndent = indent
+    }
   }
 
   def printargs[A1, B](arg1: A1)(fname: String, f: A1 => B)(implicit pr: Printer): B = {
@@ -61,13 +60,13 @@ object SubstitutionMacros {
 
   def printbody[A1, B](arg1: A1)(body: String, substring: String, f: A1 => B)(implicit pr: Printer): B = {
     val substituted = body.replace(substring, arg1.toString)
-    pr.print(substituted)
+    pr.print("= " + substituted)
     f(arg1)
   }
 
   def printres[A1, B](arg: A1)(f: A1 => B)(implicit pr: Printer): B = {
     val r = f(arg)
-    pr.print(r.toString)
+    pr.print("= " + r.toString)
     pr.indent -= 1
     r
   }
@@ -75,6 +74,16 @@ object SubstitutionMacros {
   def printall[A1, B](arg1: A1)(f: A1 => B, fname: String, body: String, substring: String)(implicit pr: Printer): B =
     printres(arg1)(x => printargs(x)(fname, y => printbody(y)(body, substring, f)))
 
+  def reduceWhitespace(str: String): String = {
+    val ws = " \n\r\t"
+    str.foldLeft(""){
+      (str, ch) =>
+        if(str.size > 0 && ws.contains(str.last) && ws.contains(ch))
+          str
+        else
+          str + (if(ws.contains(ch)) " " else ch)
+    }
+  }
 
   // TODO
   def printmatch[A](cases: List[(List[A] => Boolean, List[A] => List[A], List[A] => String, List[A] => String)])(l: List[A])(implicit pr: Printer): List[A] = {
